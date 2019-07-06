5d5bc89dafbd25b116cdd9ae26102179f7787a3e
Shaurya Dhadwal <shauryadhadwal@gmail.com>
2019-07-06 22:52:04 +0530

    Login SignUp switch button

--- client/src/pages/Auth/Auth.js
+++ client/src/pages/Auth/Auth.js
@@ -1,15 +1,20 @@
-import React, { useEffect, useRef } from 'react';
-import { Form, Button } from 'react-bootstrap';
+import React, { useState, useEffect, useRef } from 'react';
+import { Form, Button, Container, Row, Col, Tabs, Tab } from 'react-bootstrap';
 import './Auth.css';
 import Axios from 'axios';
 
 const AuthPage = () => {
 
+    const [isSignup, setIsSignup] = useState(true);
+
     const emailEl = useRef('poonamdhadwal@gmail.com');
     const passwordEl = useRef('qwerty');
+    const confirmPasswordEl = useRef('qwerty');
 
     useEffect(() => {
         emailEl.current.focus();
+        emailEl.current.value = 'shauryadhadwal@gmail.com';
+        passwordEl.current.value = 'qwerty';
     }, []);
 
     const handleSubmit = (event) => {
@@ -20,7 +25,7 @@ const AuthPage = () => {
         if (email.trim().length === 0 || password.trim().length === 0)
             return;
 
-        const body = {
+        const signUp = {
             query: `
                 mutation {
                     createUser(userInput: {email:"${email}", password:"${password}"}){
@@ -30,22 +35,32 @@ const AuthPage = () => {
                 }
             `
         };
-        Axios.post('http://localhost:3000/graphql',JSON.stringify(body), {
-            headers: {'Content-Type': 'application/json'}
-        })
-        .then(res => {
-            console.log(res);
-        })
-        .catch(err => {
-            console.log(err);
-        });
 
+        const login = {
+            query: `
+                query {
+                    login(email:"${email}", password:"${password}")
+                }
+            `
+        };
+
+        const body = isSignup ? signUp : login;
+
+        Axios.post('http://localhost:3000/graphql', JSON.stringify(body), {
+            headers: { 'Content-Type': 'application/json' }
+        })
+            .then(res => {
+                console.log(res.data);
+            })
+            .catch(err => {
+                console.log(err);
+            });
     }
 
     return (
         <div className="FormContainer">
-            <h1>Sign In Page</h1>
-            <Form onSubmit={handleSubmit}>
+            <h1>{isSignup ? 'SignUp' : 'Login'}</h1>
+            <Form onSubmit={handleSubmit} className="Form">
                 <Form.Group controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
                     <Form.Control ref={emailEl} type="email" placeholder="Enter email" required />
@@ -58,13 +73,27 @@ const AuthPage = () => {
                     <Form.Label>Password</Form.Label>
                     <Form.Control ref={passwordEl} type="password" placeholder="Password" required />
                 </Form.Group>
-                <Form.Group controlId="formBasicChecbox">
-                    <Form.Check type="checkbox" label="Remember me" />
-                </Form.Group>
+                {isSignup ?
+                    <Form.Group controlId="formBasicPassword">
+                        <Form.Label>Confirm Password</Form.Label>
+                        <Form.Control ref={confirmPasswordEl} type="password" placeholder="Password" required />
+                    </Form.Group>
+                    :
+                    null
+                }
                 <Button variant="primary" type="submit" block>
-                    Submit
+                    {isSignup ? 'SignUp' : 'Login'}
                 </Button>
             </Form>
+            <hr />
+            <p>Or switch to <span>
+                <Button variant="light" onClick={() => {
+                    setIsSignup(!isSignup)
+                }}>
+                    {isSignup ? 'Login' : 'SignUp'}
+                </Button>
+            </span>
+            </p>
         </div>
     )
 }
